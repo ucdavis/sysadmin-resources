008b6ae300ff06e60377ae2637504b8e0c30a427
bjrose <bjrose@ucdavis.edu>
2017-06-14 12:11:17 -0700

    Create DNSmonitor.sh

---
+++ DNSmonitor.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Script created by: Brian Rose
+# Email: BrianCanFixIT@Gmail.com
+# Last modified: 2017.06.17
+
+# Send alerts to this email addrress
+adminEmail="bjrose@ucdavis.edu"
+
+# Sites to monitor
+sites[0]="vpn.dss.ucdavis.edu"
+sites[1]="jss.ucdavis.edu"
+
+# DNS servers to test lookups against
+dnsServers[0]="169.237.160.10" #infoblox
+dnsServers[1]="169.237.1.250" #UCD main
+dnsServers[2]="169.237.250.250" #UCD main
+dnsServers[3]="128.120.252.9" #dns-one.ucdavis.edu
+dnsServers[4]="128.120.252.10" #dns-two.ucdavis.edu
+dnsServers[5]="192.82.111.197" #dns-three.ucdavis.edu
+dnsServers[6]="169.237.229.82" #addc6c.AD3.UCDAVIS.EDU
+dnsServers[7]="169.237.229.88" #addc9c.ad3.ucdavis.edu
+dnsServers[8]="169.237.229.83" #addc7c.AD3.UCDAVIS.EDU
+dnsServers[9]="128.120.42.42" #addc8c.ad3.ucdavis.edu
+dnsServers[10]="152.79.115.115" #ucdmc
+dnsServers[11]="152.79.105.105" #ucdmc
+dnsServers[12]="152.79.253.6" #ucdmc
+
+simpleTime=`date "+%H:%M"`
+touch dnserrorlog.txt
+
+date >> dnslookuplog.txt
+
+siteIndex=0
+siteCount=${#sites[@]}
+while [ "$siteIndex" -lt "$siteCount" ]
+do
+	for i in "${dnsServers[@]}"
+	do
+		nslookup ${sites[$siteIndex]} "$i" >> dnslookuplog.txt
+		if cat dnslookuplog.txt | grep -q NXDOMAIN
+		then
+			fulldate=`date`
+			echo "$fulldate\t\c" >> dnserrorlog.txt
+			echo "$i\twas not able to resolve the following host:\t${sites[$siteIndex]}." >> dnserrorlog.txt
+		fi
+	done
+	((siteIndex++))
+done
+
+#should do a better test here in case file does not exist, enought with cat pipe grep too
+sendError=`cat dnserrorlog.txt | grep "$simpleTime:"`
+
+if [ -n "$sendError" ]
+then
+	sendError=`echo "$sendError" && echo "" && echo "Unless listed above, all other DNS servers resolved all other hosts."`
+	echo "$sendError" | /usr/bin/mail -s "DNS is down" "$adminEmail"
+fi
+
+
+exit 0
